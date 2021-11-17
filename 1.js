e6a75c09e40f27039bfce49664abf8b25941acfe
Duminike Vika <75427769+Sweet-Day@users.noreply.github.com>
2021-11-17 19:26:12 +0200

    Завдання 1

---
+++ 1.js
@@ -0,0 +1,14 @@
+function readNumber() {
+    let num=0;
+  
+    do {
+      num = prompt("Введите число:", 0);
+    } while ( !isFinite(num) );
+  
+    if (num === null || num === '') return null;
+  
+    return +num;
+  }
+  
+  alert(`Ваше число: ${readNumber()}`);
+  
